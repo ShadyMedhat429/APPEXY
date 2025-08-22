b1be1757c7bf1634d32048e06d6022d426fda447
ShadyMedhat429 <shadesshadow68@gmail.com>
2025-08-23 02:59:46 +0300

    src/app/components/navbar/navbar.html

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, ElementRef, HostListener, inject, Renderer2, ViewChild } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -8,24 +8,45 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navbar.css'
 })
 export class Navbar {
-userMode:boolean = true
-@HostListener('click')
-ModeBtn(){
 
-  if (this.userMode == true) {
-    this.userMode = false
-    document.getElementById('lightBtn')?.classList.add('hidden')
-    document.getElementById('darkBtn')?.classList.remove('hidden')
-    document.documentElement.classList.add('dark')
 
-  } else if (this.userMode == false){
-    this.userMode = true
-    document.getElementById('lightBtn')?.classList.remove('hidden')
-    document.getElementById('darkBtn')?.classList.add('hidden')
-    document.documentElement.classList.remove('dark')
-    console.log(this.userMode)
-  }
-}  
+  mode(){
+    var isdark=true;
+if (isdark) {
+  window.document.documentElement.classList.toggle('dark')
+  window.document.getElementById('btn2')?.classList.toggle('hidden')
+  window.document.getElementById('btn')?.classList.toggle('hidden')
+}
+}
+
+//   renderer = inject(Renderer2)
+//   @ViewChild('toggle') toggle!:ElementRef 
+
+// userMode:boolean = true
+
+// ModeBtn(){
+//   this.renderer.listen(this.toggle.nativeElement,
+//     'click',()=> {
+//       console.log('Hello');
+      
+//     }
+//   )
+
+
+  // if (this.userMode == true) {
+  //   this.userMode = false
+  //   document.getElementById('lightBtn')?.classList.add('hidden')
+  //   document.getElementById('darkBtn')?.classList.remove('hidden')
+  //   document.documentElement.classList.add('dark')
+
+  // } else if (this.userMode == false){
+  //   this.userMode = true
+  //   document.getElementById('lightBtn')?.classList.remove('hidden')
+  //   document.getElementById('darkBtn')?.classList.add('hidden')
+  //   document.documentElement.classList.remove('dark')
+  //   console.log(this.userMode)
+  // }
+
 
 
 
